edc9dc233fa3e204e9fc1e7d00adb94f9c3bdad5
AjeebKt <ajeebajb@gmail.com>
2015-06-09 17:19:24 +0530

    input clean + create li

--- js/js.js
+++ js/js.js
@@ -1,7 +1,10 @@
 function add() {
-    var x = document.createElement("P");
+	// var z = document.createElement("LI")
+    var x = document.createElement("LI");
     var txt = document.getElementById('txt').value;
     var t = document.createTextNode(txt);
     x.appendChild(t);
     document.getElementById("add").appendChild(x);
+    document.getElementById("txt").value= "";
+	// document.getElementById("txt").setAttribute("placeholder","End");
 }
