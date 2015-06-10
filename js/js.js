622e1f1e7b8dffd0d8882d5599b47a67d6f800e1
AjeebKt <ajeebajb@gmail.com>
2015-06-10 17:07:38 +0530

    if is ok

--- js/js.js
+++ js/js.js
@@ -3,21 +3,17 @@
 function add() {
 	// var z = document.createElement("LI")
 	var ToDoVal = document.getElementById('txt').value;
+// console.log(ToDoVal);
 
-console.log(ToDoVal);
-
-	if (ToDoVal=="")
-	{
-
-	}
-	else
-	{
+	if (ToDoVal){
 	    var createLi = document.createElement("LI");
 	    var txtValue = document.getElementById('txt').value;
 	    var node = document.createTextNode(txtValue);
 	    createLi.appendChild(node);
 	    document.getElementById("add").appendChild(createLi);
 	    document.getElementById("txt").value= "";		
+	}else{
+		alert("please add to do in to the input field");
 	}
 	// document.getElementById("txt").setAttribute("placeholder","End");
 }
