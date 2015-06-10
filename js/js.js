719a39fc093b88c8a5fc9e1cf7f3ebd5e3aa7607
AjeebKt <ajeebajb@gmail.com>
2015-06-10 19:08:34 +0530

    js edit

--- js/js.js
+++ js/js.js
@@ -3,14 +3,17 @@
 function add() {
 	// var z = document.createElement("LI")
 	var ToDoVal = document.getElementById('txt').value;
-// console.log(ToDoVal);
+var ulList = document.getElementById("todo-list");
+
+console.log(ulList.childNodes
+	);
 
 	if (ToDoVal){
 	    var createLi = document.createElement("LI");
 	    var txtValue = document.getElementById('txt').value;
 	    var node = document.createTextNode(txtValue);
 	    createLi.appendChild(node);
-	    document.getElementById("add").appendChild(createLi);
+	    document.getElementById("ul").appendChild(createLi);
 	    document.getElementById("txt").value= "";		
 	}else{
 		alert("please add to do in to the input field");
