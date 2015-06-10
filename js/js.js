95dd0d37f5493b68e763d3aa6b99499c4e7e5f6f
AjeebKt <ajeebajb@gmail.com>
2015-06-10 15:19:31 +0530

    js readable

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 function add() {
 	// var z = document.createElement("LI")
-    var x = document.createElement("LI");
-    var txt = document.getElementById('txt').value;
-    var t = document.createTextNode(txt);
-    x.appendChild(t);
-    document.getElementById("add").appendChild(x);
+    var createLi = document.createElement("LI");
+    var txtValue = document.getElementById('txt').value;
+    var node = document.createTextNode(txtValue);
+    x.appendChild(node);
+    document.getElementById("add").appendChild(createLi);
     document.getElementById("txt").value= "";
 	// document.getElementById("txt").setAttribute("placeholder","End");
 }
