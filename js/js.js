20ad9aaf5603601ac07e567a06e3030bcfb74f92
AjeebKt <ajeebajb@gmail.com>
2015-06-10 16:11:52 +0530

    if loop

--- js/js.js
+++ js/js.js
@@ -1,10 +1,23 @@
+
+
 function add() {
 	// var z = document.createElement("LI")
-    var createLi = document.createElement("LI");
-    var txtValue = document.getElementById('txt').value;
-    var node = document.createTextNode(txtValue);
-    x.appendChild(node);
-    document.getElementById("add").appendChild(createLi);
-    document.getElementById("txt").value= "";
+	var ToDoVal = document.getElementById('txt').value;
+
+console.log(ToDoVal);
+
+	if (ToDoVal=="")
+	{
+
+	}
+	else
+	{
+	    var createLi = document.createElement("LI");
+	    var txtValue = document.getElementById('txt').value;
+	    var node = document.createTextNode(txtValue);
+	    createLi.appendChild(node);
+	    document.getElementById("add").appendChild(createLi);
+	    document.getElementById("txt").value= "";		
+	}
 	// document.getElementById("txt").setAttribute("placeholder","End");
 }
